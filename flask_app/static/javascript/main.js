5440946b696a0535c5849130ae25662f977f338e
DESKTOP-UK2RI9P\Matt Joga <matthewjoga@outlook.com>
2022-03-27 18:50:48 -0400

    My-Virtual-Binder

--- flask_app/static/javascript/main.js
+++ flask_app/static/javascript/main.js
@@ -5,3 +5,22 @@ function addShadow(element){
 function removeShadow(element){
     element.classList.remove("shadow");
 }
+
+$("#submit-button").on("click", function (event){
+    $("#card-container").empty();
+    event.preventDefault();
+    var pokemon = $("#search_bar")
+    .val()
+    .trim();
+
+    $.ajax({
+        method:"GET",
+        url: "https://api.pokemontcg.io/v1/cards/?name=" + pokemon
+    }).then(function(response){
+        for (var i = 0; i < response.cards.length; i++){
+            var pokemonCard = $("<img class='pkmn-card' onmouseover='addShadow(this)' onmouseleave='removeShadow(this)'>");
+            pokemonCard.attr("src", response.cards[i].imageUrlHiRes);
+            $("#card-container").append(pokemonCard);
+        }
+    });
+});
